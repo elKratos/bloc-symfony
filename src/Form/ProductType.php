32dfb11af6886ae45c5dfc44703f3a79132c1732
elKratos <elkratos42@gmail.com>
2020-07-08 02:15:46 +0200

    Gestionar permisos de la aplicacion. Invalidar los botones a los que solo el admin puede acceder.

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,8 +6,11 @@ use App\Entity\Product;
 use App\Entity\TypeFlower;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProductType extends AbstractType
 {
@@ -16,11 +19,38 @@ class ProductType extends AbstractType
         $builder
             ->add('title')
             ->add('description')
-            ->add('publishedAt')
-            ->add('urlImg')
-            ->add('typeFlower', EntityType::class,
-                ['class' => TypeFlower::class, 'choice_label' => 'name']
-            )
+            ->add('publishedAt', DateType::class, [
+                'format' => 'yyyy-MM-dd',
+                'widget' => 'single_text',
+            ])
+            ->add('urlImg', FileType::class, [
+                'label' => 'Image (format: .jpg, .png)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '512k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png'
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid file',
+                    ])
+                ],
+
+            ])
+            ->add('typeFlower', EntityType::class, [
+                'class' => TypeFlower::class, 'choice_label' => 'name'
+            ])
         ;
     }
 
